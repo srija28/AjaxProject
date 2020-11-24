c1e9d5c0b1927f2e7f390b3d25bd47d212a109b9
shreya <shreyareddykethireddy@shreyas-MacBook-Air-2.local>
2020-11-25 02:21:05 +0530

    [Shreya]Added: test services

---
+++ js/http_service.js
@@ -0,0 +1,29 @@
+function makePromiseCall(methodType, url, async=true, data=null){
+    return new Promise(function (resolve, reject){
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        console.log(methodType+" State Change Called. Ready State: "+ xhr.readyState + " Status : "+ xhr.status);
+        if(xhr.status.toString().match("^[2][0-9]{2}$")){
+            resolve(xhr.responseText);
+        }else if(xhr.status.toString().match("^[4,5][0-9]{2}$")){
+            reject({
+               status: xhr.status,
+               statusText: xhr.statusText
+            }); 
+        console.log("XHR Failed");
+        }
+    }
+    xhr.onerror = function(){
+        reject({
+            status: xhr.status,
+            statusText: xhr.statusText
+        });
+    };
+    xhr.open(methodType, url, async);
+    if(data){
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    }else xhr.send();
+    console.log("Request sent to server");
+});
+}
